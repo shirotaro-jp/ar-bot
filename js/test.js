af16d8165fe1be651a864dd4d5533dc6dec005db
shirotaro-jp <shirotaro.jp@gmail.com>
2019-01-27 14:23:54 +0900

    おじさんの上にクリックできる要素を追加するとカメラが動かない

--- js/test.js
+++ js/test.js
@@ -356,21 +356,21 @@ $(document).ready(function(){
   // ギフトボタンクリック時
   $( "#present" ).click(function() {
     
-    // // サウンドを読み込む
-    // // どの音を再生するか
-    // var random = Math.floor( Math.random() * 3 ); //0~2
-    // var sound = "";
-    // sound = 'wav/beer'+random+'.wav';
-    // //0:えっ。いいとっ。ありがと
-    // //1:ビール、一緒に飲もうよ
-    // //2:ありが唐辛子
-    // //3:
-    // //4:
-    // getAudioBuffer(sound, function(buffer) {
-    //   console.log('play');
-    //   // コールバックを実行
-    //   playSound(buffer);
-    // });
+    // サウンドを読み込む
+    // どの音を再生するか
+    var random = Math.floor( Math.random() * 3 ); //0~2
+    var sound = "";
+    sound = 'wav/beer'+random+'.wav';
+    //0:えっ。いいとっ。ありがと
+    //1:ビール、一緒に飲もうよ
+    //2:ありが唐辛子
+    //3:
+    //4:
+    getAudioBuffer(sound, function(buffer) {
+      console.log('play');
+      // コールバックを実行
+      playSound(buffer);
+    });
 
     var gel = document.querySelector('#gift-object');
     gel.setAttribute('visible', true);
@@ -380,26 +380,26 @@ $(document).ready(function(){
 
   });
 
-  // // おじさんクリック時
-  // $( "#oldman" ).click(function() {
+  // おじさんクリック時
+  $( "#oldman" ).click(function() {
     
-  //   // サウンドを読み込む
-  //   // どの音を再生するか
-  //   var random = Math.floor( Math.random() * 5 ); //0~4
-  //   var sound = "";
-  //   sound = 'wav/touch'+random+'.wav';
-  //   //0:優しく触ってね
-  //   //1:好きよねぇ。ボディータッチ。
-  //   //2:今日も綺麗だねぇ。
-  //   //3:今日も可愛いねぇ。
-  //   //4:もっと近づいて欲しいなぁ。
-  //   getAudioBuffer(sound, function(buffer) {
-  //     console.log('play');
-  //     // コールバックを実行
-  //     playSound(buffer);
-  //   });
+    // サウンドを読み込む
+    // どの音を再生するか
+    var random = Math.floor( Math.random() * 5 ); //0~4
+    var sound = "";
+    sound = 'wav/touch'+random+'.wav';
+    //0:優しく触ってね
+    //1:好きよねぇ。ボディータッチ。
+    //2:今日も綺麗だねぇ。
+    //3:今日も可愛いねぇ。
+    //4:もっと近づいて欲しいなぁ。
+    getAudioBuffer(sound, function(buffer) {
+      console.log('play');
+      // コールバックを実行
+      playSound(buffer);
+    });
 
-  // });
+  });
 
 
   // ** Start #1 **
